9e583cd97897bdef161a17d1772c6f51d40cf76c
Jez-WorkShop <jezworkshop@gmail.com>
2025-06-19 19:24:51 +0200

    Add compile and run scripts for C++ exercises

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make cpp_clean
+make cpp_compile
+
+make ts_clean
+make ts_compile
---
+++ run_cpp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "You must specify an exercise number"
+  exit 1
+fi
+
+make cpp_run ARGS="$1"
